f819ac82ef88df61945f2ca9485a7dd43cc5f930
AlissonSouzaDev <alisson.oliveira2874@gmail.com>
2024-08-20 16:33:01 -0300

    feat: create table

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SimpleTabs from "./components/tabs";
+import SimpleTable from "./components/table";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <SimpleTabs />
+      <SimpleTable />
     </main>
   );
 }
