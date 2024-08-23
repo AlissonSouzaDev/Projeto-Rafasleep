c84f0f8ac4765593f11a96f610214fd2dd335d1a
AlissonSouzaDev <alisson.oliveira2874@gmail.com>
2024-08-22 22:36:23 -0300

    feat: make api request

---
+++ pages/api/movies/popular.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const apiKey = process.env.TMDB_API_KEY;
+
+  console.log('TMDB_API_KEY:', process.env.TMDB_API_KEY);
+
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular`, {
+      params: {
+        api_key: apiKey,
+        language: 'pt-BR',
+        page: 1,
+      },
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log('Showala', apiKey || 'API key is undefined');
+    res.status(500).json({ error: 'Erro ao buscar filmes populares1' });
+  }
+}
